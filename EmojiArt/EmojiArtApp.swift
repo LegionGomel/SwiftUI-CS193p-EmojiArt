023720e78dc0d13f3c9232170abefda1aa4468a3
Yuri Ershov <shtorm0606@gmail.com>
2021-12-17 14:01:25 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Yuri Ershov on 17.12.21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
